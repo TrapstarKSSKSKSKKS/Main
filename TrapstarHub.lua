38fa43d7cc629166d8c1142e3d514bc278c4f1a0
TrapstarKSSKSKSKKS <101758758+TrapstarKSSKSKSKKS@users.noreply.github.com>
2022-04-20 21:22:14 -0300

    Add files via upload

---
+++ TrapstarHub.lua
@@ -0,0 +1,9 @@
+ --Grand Piece Online
+ --DeepWoken
+ --Blox Fruit
+ --RoGhoul
+ --AnimeFighters
+ --AllStarTowerDefense
+ --DemonFall
+ --Arsenal
+loadstring(game:HttpGet("https://pastebin.com/raw/7rSb1Xa9"))()
