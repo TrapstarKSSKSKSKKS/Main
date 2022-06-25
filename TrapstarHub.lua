7b435bf96c866cdbd0b53d727799559ba4eb6e26
TrapstarKSSKSKSKKS <101758758+TrapstarKSSKSKSKKS@users.noreply.github.com>
2022-06-24 23:41:08 -0300

    Delete TrapstarHub.lua

--- TrapstarHub.lua
+++
@@ -1,9 +0,0 @@
- --Grand Piece Online
- --DeepWoken
- --Blox Fruit
- --RoGhoul
- --AnimeFighters
- --AllStarTowerDefense
- --DemonFall
- --Arsenal
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TrapstarKSSKSKSKKS/Main/main/Hub.lua"))()
