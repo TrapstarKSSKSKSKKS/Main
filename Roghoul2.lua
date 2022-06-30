d9888e030dc086ad183f368cf71bfc2adf76fa0c
TrapstarKSSKSKSKKS <101758758+TrapstarKSSKSKSKKS@users.noreply.github.com>
2022-06-30 16:27:43 -0300

    Update Roghoul2.lua

--- Roghoul2.lua
+++ Roghoul2.lua
@@ -18,6 +18,13 @@ local player = get.Players.LocalPlayer
 
 repeat wait() until player:FindFirstChild("PlayerFolder")
 
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
 local team, remotes, stat = player.PlayerFolder.Customization.Team.Value, get.ReplicatedStorage.Remotes, player.PlayerFolder.StatsFunction
 local oldtick, farmtick = 0, 0
 local camera = workspace.CurrentCamera
